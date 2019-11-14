5c0663a3a9e17484cc2533b15cd046aba3ed77d4
zied <zalghamd@asu.edu>
2019-11-13 17:47:40 -0700

    added the code to test it

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    struct Course: Decodable {
+        let id: Int
+        let name: String
+        let link: String
+        let imageUrl: String
+        
+        //constructor
+        init (json: [String: Any]){
+            id = json["id"] as? Int ?? -1
+            name = json["name"] as? String ?? ""
+            link = json["link"] as? String ?? ""
+            imageUrl = json["imageUrl"] as? String ?? ""
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
+        let jsonUrlString =  "https://api.letsbuildthatapp.com/jsondecodable/course"
+               
+        //https://l4gv9uqwpd.execute-api.us-west-1.amazonaws.com/prod/sensordata
+               
+              
+       //url constructor
+       guard let url = URL(string: jsonUrlString) else {return}
+               
+       //url session to fetch the data
+       URLSession.shared.dataTask(with: url) { (data, response, err) in
+                   
+       //get data from the url
+       guard let data = data else{return}
+        
+        
+        //serilize the data
+        do{
+                       
+           let course = try JSONDecoder().decode(Course.self, from: data)
+         
+            }catch let jsonErr{
+            print("error serializing json:", jsonErr)
+           }
+          
+               }.resume() //start the url session
+   
     }
 
 
